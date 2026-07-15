2d2bb809a5520beaaf4322cd254d0de534f40f7f
elnomic <elnomic@yahoo.com>
2026-07-15 12:01:01 +0700

    Create Layout component with Header and Navigation

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,23 @@
+// src/components/Layout/Layout.tsx
+import { Header } from './Header'
+import { Navigation } from './Navigation'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex flex-1">
+        <Navigation />
+        <main className="flex-1 p-6 overflow-auto">
+          <div className="max-w-7xl mx-auto">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
